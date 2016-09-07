6c319bbbe186136551b6954d64ebe2e43779deed
Your Name <you@example.com>
2016-09-07 15:23:03 +0200

    Verzija bez tabele.

--- Zad_sekvenca.c
+++ Zad_sekvenca.c
@@ -2,7 +2,6 @@
 #include <math.h>
 #include <stdlib.h>
 int izracunaj(int *b) {
-printf(" = sequence number\n");
 (*b)++;
 return *b;
 }
@@ -12,14 +11,21 @@ int n, b = 0;
 
 printf("Unesi broj:");
 scanf("%d",&n);
-
+printf("\t+-------+ +-------+");
+printf("\n\t|Inter. | | Brojac|\n");
+printf("\t+-------+ +-------+\n");
  while (n !=1) {
   if (n%2==0) {
     n=n/2;
     izracunaj(&b);
-    printf("b=%d",b); }
+
+    printf("\t| %5d | | %5d |\n", b, n); }
+
   else {
     n = 3 * n + 1;
     izracunaj(&b);
-    printf ("b=%d",b);}
-}}
+    printf("\t| %5d | | %5d |\n", b, n); }
+
+  }
+  
+}
